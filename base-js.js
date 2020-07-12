a9b6e677e641f2195b1352a392400fa811811c13
liubin-718 <liubin718@163.com>
2020-07-12 22:18:28 +0800

    【web-js：example】  1.this   2.js方法

--- base-js.js
+++ base-js.js
@@ -38,3 +38,54 @@ function trim(v) {
     return v.replace(/^\s*/, '').replace(/\s*$/, '')
 }
 
+// 3、实现Array.prototype.map
+// map()方法创建一个新数组，其结果是该数组中的每个元素都调用一个提供的函数后返回的结果
+function map(arr, mapCb) {
+    // 检查传参
+    if (!Array.isArray(arr) || !arr.length || typeof mapCb !== 'function') {
+        return []
+    } else {
+        let result = []
+        // 每次调用此函数时，都创建一个result数组，我们不想改变原始数组
+        for (let i = 0, len = arr.length; i < len; i++) {
+            // 将mapCb返回的结果push到result数组中
+            result.push(mapCb(arr[i], i, arr))
+        }
+        return result
+    }
+}
+// 4、实现Array.prototype.filter
+// filter()创建一个新数组，其包含通过所提供函数实现的测试的所有元素
+function filter(arr, filterCb) {
+    if (!Array.isArray(arr) || !arr.length || typeof filterCb !== 'function') {
+        return []
+    } else {
+        let result = []
+        for (let i = 0, len = arr.length; i < len; i++) {
+            // 检查filterCb的返回值是否是真值
+            if (filterCb(arr[i], i, arr)) {
+                // 条件为真，则将数组元素push到result
+                result.push(arr[i])
+            }
+        }
+        return result
+    }
+}
+
+// 5、Array.prototype.reduce
+// reduce()对数组中的每个元素执行一个由您提供的reducer函数（升序执行），将其结果汇总为单个返回值
+function reduce(arr, reduceCb, initialValue) {
+    if (!Array.isArray(arr) || !arr.length || typeof reduceCb !== 'function') {
+        return []
+    } else {
+        // 如果没有将initialValue传递给函数，我们将数组第一项作为initialValue
+        let hasInitialValue = initialValue !== undefined
+        let value = hasInitialValue ? initialValue : arr[0]
+        // 如果有传递initialValue,则索引从1开始，否则从0开始
+        for (let i = hasInitialValue ? 0 : 1, len = arr.length; i < len; i++) {
+            value = reduceCb(value, arr[i], i, arr)
+        }
+        return value
+    }
+}
+
--- this.js
+++ this.js
@@ -32,3 +32,25 @@ const { printName1 } = logger1
 printName1()
 
 
+/* 1、在浏览器里，在全局范围内this指向window对象；
+2、在函数中，this永远指向最后调用他的那个对象；
+3、构造函数中，this指向new出来的那个新的对象；
+4、call、apply、bind中的this被强绑定在指定的那个对象上；
+5、箭头函数中this比较特殊，箭头函数this为父作用域的this，不是调用时的this。要知道前四种方式都是调用时确定，
+也就是动态的，而箭头函数的this指向的是静态的，生命的时候就确定下来。
+6、apply、call、bind都是js给函数内置的一些api，调用他们可以为函数指定this的执行，同时也可以传参。 */
+const data = {
+    result: 0,
+    nums: [1, 2, 3, 4, 5],
+    computeResult() {
+        // 这里的this指的是data对象
+        const addAll = () => {
+            return this.nums.reduce((total, cur) => total + cur, 0)
+        }
+        this.result = addAll()
+    },
+}
+// 箭头函数没有自己的this，它捕获词法作用域函数的this值，在此示例中，addAll函数将复制computeResult方法中的this值，
+// 如果我们在全局作用域声明箭头函数，则this值为window对象。
+
+
